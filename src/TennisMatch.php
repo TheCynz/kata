5c7e77d17fe9143087d68fdaaf6ed8e13b767a32
Tom Jones <tomcjones@hotmail.co.uk>
2022-05-11 21:55:00 +0100

    add Tennis Match

---
+++ src/TennisMatch.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App;
+
+class TennisMatch
+{
+    protected $playerOnePoints = 0;
+    protected $playerTwoPoints = 0;
+
+
+    public function score()
+    {
+        if ($this->hasWinner()) {
+            return 'Winner: ' . $this->leader();
+        }
+
+        //check for deuce
+        if ($this->isDeuce()) {
+            return 'deuce';
+        }
+
+        // def
+        return sprintf(
+            "%s-%s",
+            $this->pointsToTerm($this->playerOnePoints),
+            $this->pointsToTerm($this->playerTwoPoints),
+        );
+    }
+
+
+
+    public function pointToPlayerOne()
+    {
+        $this->playerOnePoints++;
+    }
+
+    public function pointToPlayerTwo()
+    {
+        $this->playerTwoPoints++;
+    }
+
+    public function pointsToTerm($points)
+    {
+        switch ($points) {
+            case 0:
+                return 'love';
+            case 1:
+                return 'fifteen';
+            case 2:
+                return 'thirty';
+            case 3:
+                return 'forty';
+        }
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasWinner(): bool
+    {
+        if ($this->playerOnePoints > 3 && ($this->playerOnePoints >= $this->playerTwoPoints + 2)) {
+            return true;
+        }
+
+        if ($this->playerTwoPoints > 3 && ($this->playerTwoPoints >= $this->playerOnePoints + 2)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * @return string
+     */
+    public function leader(): string
+    {
+        return $this->playerOnePoints > $this->playerTwoPoints
+            ? 'Player 1'
+            : 'Player 2';
+    }
+
+    /**
+     * @return bool
+     */
+    public function isDeuce(): bool
+    {
+        return $this->playerOnePoints >= 3 && $this->playerTwoPoints >= 3 && $this->playerOnePoints === $this->playerTwoPoints;
+    }
+}
---
+++ tests/TennisMatchTest.php
@@ -0,0 +1,45 @@
+<?php
+
+use App\TennisMatch;
+use PHPUnit\Framework\TestCase;
+
+class TennisMatchTest extends TestCase
+{
+    /** @test
+     * @dataProvider scores
+     */
+
+    function it_scores_a_tennis_match($playerOnePoints, $playerTwoPoints, $score)
+    {
+        $match = new TennisMatch();
+
+        for ($i = 0; $i < $playerOnePoints; $i++) {
+            $match->pointToPlayerOne();
+        }
+
+        for ($i = 0; $i < $playerTwoPoints; $i++) {
+            $match->pointToPlayerTwo();
+        }
+        $this->assertEquals($score, $match->score());
+    }
+
+    public function scores()
+    {
+        return [
+            [0,0,'love-love'],
+            [1,0,'fifteen-love'],
+            [1,1,'fifteen-fifteen'],
+            [2,0,'thirty-love'],
+            [3,0,'forty-love'],
+            [2,2,'thirty-thirty'],
+            [3,3,'deuce'],
+            [4,4,'deuce'],
+            [5,5,'deuce'],            [6,6,'deuce'],
+            [7,7,'deuce'],
+            [4,0,'Winner: Player 1'],
+            [0,4,'Winner: Player 2']
+        ];
+    }
+
+
+}
